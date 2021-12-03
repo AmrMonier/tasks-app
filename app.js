85c3d52130acfceca8535ad2e02c79f5bcc54453
Amr Monier <a.monier2107@gmail.com>
2021-12-03 15:53:17 +0200

    intialized the express server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import mongoose from './src/middlewares/mongoose.js';
-
+import express from 'express'
+import bodyParser from "body-parser";
+import cors from "cors";
 mongoose.then(() => {
     console.log('connected to the Database');
+    const app = express()
+    const PORT = process.env.PORT || 5000
+    app.use(bodyParser.json())
+    app.use(cors())
+    app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
 }).catch(err => {
     console.log('failed to connect to the database');
 })

d474759aef7a63329f6e494587f9794de5868307
Amr Monier <a.monier2107@gmail.com>
2021-12-03 17:22:19 +0200

    added the read, delete, update functionality of tasks

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -7,6 +7,40 @@ class TaskController {
       else return res.status(201).json(user);
     });
   }
+
+  async index(req, res) {
+    Task.find({},(err, tasks) => {
+      if (err) return res.status(400).json(err);
+      else return res.status(200).json(tasks);
+    })
+  }
+  async read(req, res)  {
+    const _id = req.params.id
+    Task.findById(_id, (err, task) => {
+      if (err) return res.status(400).json(err);
+      else if (task) return res.status(200).json(task);
+      else return res.status(404).json()
+    })
+  }
+
+  async update(req, res){
+    const data = req.body
+    const _id = req.params.id
+    Task.findByIdAndUpdate(_id, data, {new: true},(err, task, x) => {
+      if (err) return res.status(400).json(err);
+      else if (task) return res.status(202).json(task);
+      else return res.status(404).json()
+    })
+  }
+
+  async delete (req, res){
+    const _id = req.params.id
+    Task.findByIdAndDelete(_id, (err, task)=> {
+      if (err) return res.status(400).json(err);
+      else if (task) return res.status(204).json();
+      else return res.status(404).json()
+    })
+  }
 }
 
 export default new TaskController();
--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import TaskController from '../controllers/TaskController.js';
+import { Router } from "express";
+import TaskController from "../controllers/TaskController.js";
 
 const routes = new Router();
 
-routes.post('/', TaskController.create);
+routes.post("/", TaskController.create);
+routes.get("/", TaskController.index);
+routes.get("/:id", TaskController.read);
+routes.put("/:id", TaskController.update);
+routes.delete("/:id", TaskController.delete);
 
 export default routes;

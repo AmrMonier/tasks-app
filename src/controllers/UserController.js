2a63e30c4dfcc27c0c15812f53d5a7a0574a3cc5
Amr Monier <a.monier2107@gmail.com>
2021-12-03 16:12:21 +0200

    added the ability to fetch all the users from the database

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,6 +7,13 @@ class UserController {
       else return res.status(201).json(user);
     });
   }
+
+  async index(req, res) {
+    User.find({},(err, users) => {
+      if (err) return res.status(400).json(err);
+      else return res.status(200).json(users);
+    })
+  }
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,6 @@ import UserController from '../controllers/UserController.js';
 const routes = new Router();
 
 routes.post('/', UserController.create);
+routes.get('/', UserController.index);
 
 export default routes;

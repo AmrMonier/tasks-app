cfbf6e35ba7b34ded39325ef75843e577501898b
Amr Monier <a.monier2107@gmail.com>
2021-12-06 23:56:20 +0200

    added maintainance miidleware

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-import mongoose from './src/middlewares/mongoose.js';
-import express from 'express'
+import mongoose from "./src/middlewares/mongoose.js";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import userRoutes from './src/routes/userRoutes.js'
-import tasksRoutes from './src/routes/tasksRoutes.js'
-import authenticationRoutes from './src/routes/authenticationRoutes.js'
+import userRoutes from "./src/routes/userRoutes.js";
+import tasksRoutes from "./src/routes/tasksRoutes.js";
+import authenticationRoutes from "./src/routes/authenticationRoutes.js";
 
-mongoose.then(() => {
-    console.log('connected to the Database');
-    const app = express()
-    const PORT = process.env.PORT || 5000
-    app.use(bodyParser.json())
-    app.use(cors())
-    app.use('/users', userRoutes)
-    app.use('/tasks', tasksRoutes)
-    app.use('/auth', authenticationRoutes)
+mongoose
+  .then(() => {
+    console.log("connected to the Database");
+    const app = express();
+    const PORT = process.env.PORT || 5000;
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use((req, res, next) => {
+      if (process.env.ENVIROMENT === "maintainance")
+        return res.status(503).json({ msg: "server under maintainance" });
+      else next();
+    });
+    app.use("/users", userRoutes);
+    app.use("/tasks", tasksRoutes);
+    app.use("/auth", authenticationRoutes);
 
-    app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
-}).catch(err => {
-    console.log('failed to connect to the database');
-})
+    app.listen(PORT, () =>
+      console.log(`App running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log("failed to connect to the database");
+  });

58d647c62441ba27d1e9c105df53ca9a439001ae
Amr Monier <a.monier2107@gmail.com>
2021-12-06 23:23:10 +0200

    added jwt to the login response

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -1,11 +1,13 @@
 import User from "../models/User.js";
+import cryptography from "../utilities/cryptography.js";
 
 class AuthenticationController {
   async login(req, res) {
     const data = req.body
     try {
       const user = await User.findByCredentials(data.email, data.password)
-      return res.json(user)
+      let token = await cryptography.generateJwtToken(user.toObject())
+      return res.json({user, token})
     } catch (error) {
       console.log(error);
       return res.status(400).json({err: 'invalid credentials'})

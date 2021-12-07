63e472a232e844bf9afafc671afe1ba2f8f89fc8
Amr Monier <a.monier2107@gmail.com>
2021-12-07 14:36:09 +0200

    modified the authenticated middleware to fetch the user document from the database

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,16 +1,23 @@
 import cryptography from '../utilities/cryptography.js'
+import User from '../models/User.js'
 export default {
      isAuthenticated: async (req, res, next) => {
         let token = req.headers.authorization.split(' ')[1]
         try {
             let user = await cryptography.verifyJwtToken(token) 
             if(user){
-                req.user = user
-                next()
+                
+                user = await User.findById(user._id)
+                if (user) {
+                    req.user = user
+                    next()
+                }else{
+                    throw new Error
+                }
             }
             
         } catch (error) {
-            return res.status(401).json({msg: 'invalid token'})
+            return res.status(401).json({msg: 'please re-authenticate'})
         }
     }
 }

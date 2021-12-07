ccdca839c5019db07bb072f071fba01622efa247
Amr Monier <a.monier2107@gmail.com>
2021-12-07 10:20:11 +0200

    fixed a typo and wrapped all response objects with {}

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -2,23 +2,23 @@ import Task from "../models/Task.js";
 class TaskController {
   async create(req, res) {
     const data = req.body;
-    Task.create(data, (err, user) => {
+    Task.create(data, (err, task) => {
       if (err) return res.status(400).json(err);
-      else return res.status(201).json(user);
+      else return res.status(201).json({task});
     });
   }
 
   async index(req, res) {
     Task.find({},(err, tasks) => {
       if (err) return res.status(400).json(err);
-      else return res.status(200).json(tasks);
+      else return res.status(200).json({tasks});
     })
   }
   async read(req, res)  {
     const _id = req.params.id
     Task.findById(_id, (err, task) => {
       if (err) return res.status(400).json(err);
-      else if (task) return res.status(200).json(task);
+      else if (task) return res.status(200).json({task});
       else return res.status(404).json()
     })
   }
@@ -28,7 +28,7 @@ class TaskController {
     const _id = req.params.id
     Task.findByIdAndUpdate(_id, data, {new: true},(err, task, x) => {
       if (err) return res.status(400).json(err);
-      else if (task) return res.status(202).json(task);
+      else if (task) return res.status(202).json({task});
       else return res.status(404).json()
     })
   }

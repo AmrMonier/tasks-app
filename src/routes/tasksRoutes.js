dd41cdbd3dd2c99738130db2defce158e64b93a3
Amr Monier <a.monier2107@gmail.com>
2021-12-07 10:18:22 +0200

    protected the taskRoutes with Authentication

--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import TaskController from "../controllers/TaskController.js";
+import authentication from "../middlewares/authentication.js";
 
 const routes = new Router();
 
-routes.post("/", TaskController.create);
-routes.get("/", TaskController.index);
-routes.get("/:id", TaskController.read);
-routes.put("/:id", TaskController.update);
-routes.delete("/:id", TaskController.delete);
+routes.post("/", authentication.isAuthenticated, TaskController.create);
+routes.get("/", authentication.isAuthenticated, TaskController.index);
+routes.get("/:id", authentication.isAuthenticated, TaskController.read);
+routes.put("/:id", authentication.isAuthenticated, TaskController.update);
+routes.delete("/:id", authentication.isAuthenticated, TaskController.delete);
 
 export default routes;

c4f0b2fa7daedf2c599361c413b2cd8990ac0cf2
Amr Monier <a.monier2107@gmail.com>
2021-12-10 22:25:39 +0200

    added markdown documentation as the home page

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from 'path'
+import expressMd from "express-md";
+ import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import userRoutes from "./src/routes/userRoutes.js";
 import tasksRoutes from "./src/routes/tasksRoutes.js";
@@ -13,6 +19,7 @@ mongoose
     console.log("connected to the Database");
     const app = express();
     const PORT = process.env.PORT || 5000;
+    const mdRouter = expressMd({dir: __dirname, url: '/'})
     app.use(bodyParser.json());
     app.use(cors());
     app.use((req, res, next) => {
@@ -20,7 +27,7 @@ mongoose
         return res.status(503).json({ msg: "server under maintainance" });
       else next();
     });
-    
+    app.use(mdRouter)
     app.use("/users", userRoutes);
     app.use("/tasks", tasksRoutes);
     app.use("/auth", authenticationRoutes);
